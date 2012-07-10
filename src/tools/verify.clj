c9c4051a929b39d91c701f18bf7b6b8182f6c499
John Sefler <jsefler@redhat.com>
2012-07-10 15:23:56 -0300

    Update master

--- src/tools/verify.clj
+++ src/tools/verify.clj
@@ -33,6 +33,8 @@
            form# '~x]
        (check res# form# (used-bindings ~bindings form#) @err#))))
 
+(def verify verify-that)
+
 (defn verify-every [pred coll]
   (let [outcoll (map (fn [x] [x (pred x)]) coll)
         passed? #(identity (second %))]
